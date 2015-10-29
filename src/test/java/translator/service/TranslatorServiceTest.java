42a7ba6b1e564bd9c7079c735b91d47959ad422a
David <nicu1309@gmail.com>
2015-10-29 12:18:12 +0100

    First trycommit gradle works

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "one does not simply commits this code");
+        assertEquals("uno no simplemente comete este código",translatedText.getTranslation());
     }
 
 }
--- src/test/java/translator/web/ws/TranslatorEnpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEnpointTest.java
@@ -42,11 +42,11 @@ public class TranslatorEnpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("one does not simply commits this code");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("uno no simplemente comete este código"));
 	}	
 }
